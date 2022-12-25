80246f41e379df9ccf3999bf5139deea4e082f01
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-12-25 22:49:33 +0300

    Add try and catch error to resolver.js

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -5,29 +5,6 @@ const resolvers = {
       return dataSources.trackAPI.getTracksForHome();
     },
 
-    Mutation: {
-      // incrementa a track's numberOfViews property
-      incrementTrackViews: async (_, { id }, { dataSources }) => {
-        try {
-          const track = await dataSources.trackAPI.incrementTrackViews(id);
-
-          return {
-            code: 200,
-            success: true,
-            message: `Successfully incremented number of views for track ${id}`,
-            track,
-          };
-        } catch (err) {
-          return {
-            code: err.extensions.response.status,
-            success: false,
-            message: err.extensions.response.body,
-            track: null,
-          };
-        }
-      },
-    },
-
     // get a single track by ID, for the track page
     track: (_, { id }, { dataSources }) => {
       return dataSources.trackAPI.getTrack(id);
@@ -45,6 +22,29 @@ const resolvers = {
 
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
+    },
+  },
+
+  Mutation: {
+    // incrementa a track's numberOfViews property
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      } catch (err) {
+        return {
+          code: err.extensions.response.status,
+          success: false,
+          message: err.extensions.response.body,
+          track: null,
+        };
+      }
     },
   },
 };
