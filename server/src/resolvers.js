466a322e41ade33a9bde6cb4988a72ad81e27670
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-12-25 21:47:07 +0300

    Add incrementTrackViews

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -5,6 +5,20 @@ const resolvers = {
       return dataSources.trackAPI.getTracksForHome();
     },
 
+    Mutation: {
+      // incrementa a track's numberOfViews property
+      incrementTrackViews: async (_, { id }, { dataSources }) => {
+        const track = await dataSources.trackAPI.incrementTrackViews(id);
+
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track,
+        };
+      },
+    },
+
     // get a single track by ID, for the track page
     track: (_, { id }, { dataSources }) => {
       return dataSources.trackAPI.getTrack(id);
